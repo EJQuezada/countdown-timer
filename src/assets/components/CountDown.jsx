1c2ec985d3f9fa100967047b6d5bc5519549ff2d
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-16 11:52:39 +0200

    create project and countdown.jsx file

---
+++ src/assets/components/CountDown.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CountdownTimer = () => {
+
+    return (
+        <div className="countdown-timer-container">
+
+        </div>
+    );
+};
+
+export default CountdownTimer;
